c81a2a74f9894a7ac41dce0a9b592030418a2bbb
Daniel Micay <danielmicay@gmail.com>
2017-07-24 22:59:05 +0200

    Stop granting location to Browser app by default
    It works fine without it and requests it after the user grants
    location access to a site.
    
    Change-Id: Ifabc3f1ae4acf008abf1467fc928eeb90613feff

--- services/core/java/com/android/server/pm/permission/DefaultPermissionGrantPolicy.java
+++ services/core/java/com/android/server/pm/permission/DefaultPermissionGrantPolicy.java
@@ -681,7 +681,7 @@ public final class DefaultPermissionGrantPolicy {
                         Intent.CATEGORY_APP_EMAIL, userId),
                 userId, CONTACTS_PERMISSIONS, CALENDAR_PERMISSIONS);
 
-        // Browser
+        /* Browser
         String browserPackage = ArrayUtils.firstOrNull(getKnownPackages(
                 PackageManagerInternal.PACKAGE_BROWSER, userId));
         if (browserPackage == null) {
@@ -691,8 +691,9 @@ public final class DefaultPermissionGrantPolicy {
                 browserPackage = null;
             }
         }
-        grantPermissionsToPackage(pm, browserPackage, userId, false /* ignoreSystemPackage */,
-                true /*whitelistRestrictedPermissions*/, FOREGROUND_LOCATION_PERMISSIONS);
+        grantPermissionsToPackage(pm, browserPackage, userId, false, true,
+                FOREGROUND_LOCATION_PERMISSIONS);
+        */
 
         // Voice interaction
         if (voiceInteractPackageNames != null) {
