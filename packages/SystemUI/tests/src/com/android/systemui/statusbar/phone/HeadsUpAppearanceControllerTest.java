687dced9286d25ef883f92cc51a529f206665313
Sam Mortimer <sam@mortimer.me.uk>
2018-01-13 23:26:48 -0800

    SystemUI: Network Traffic [1/3]
    *) Add lineage-sdk internal NetworkTraffic to the statusbar
       using the LineageStatusBarItemHolder helper.
    
    Author: LuK1337 <priv.luk@gmail.com>
    Date:   Thu May 6 13:50:05 2021 +0200
    
        SystemUI: Force left-to-right for network traffic monitor holder
    
        Fixes: https://gitlab.com/LineageOS/issues/android/-/issues/3445
        Change-Id: I19ea852cedfd2a69a47006977f1b74a172008058
    
    Author: LuK1337 <priv.luk@gmail.com>
    Date:   Sun Apr 11 09:58:09 2021 +0200
    
        SystemUI: Hide centered_area when showing heads up
    
        Needed for network traffic monitor.
    
        Change-Id: Ib96a75a936cf4d71e23a575e012741b812ed2aa1
    
    Author: LuK1337 <priv.luk@gmail.com>
    Date:   Sun Apr 11 09:47:17 2021 +0200
    
        SystemUI: Move network traffic monitor under same layout as clock
    
        This effectively fixes wrongly network monitor layout on
        devices with side notch / hole punch.
    
        Change-Id: I217b886d8da75eab7636066f4822f5fd6dbc9aff
    
    Author: Sam Mortimer <sam@mortimer.me.uk>
    Date:   Sat Jan 13 23:26:48 2018 -0800
    
        SystemUI: Network Traffic [1/3]
    
        *) Add lineage-sdk internal NetworkTraffic to the statusbar
           using the LineageStatusBarItemHolder helper.
    
        Change-Id: Ib947832860970a3bccfac70d27a4761f6164d3d5
    
    Change-Id: Ib947832860970a3bccfac70d27a4761f6164d3d5

--- packages/SystemUI/tests/src/com/android/systemui/statusbar/phone/HeadsUpAppearanceControllerTest.java
+++ packages/SystemUI/tests/src/com/android/systemui/statusbar/phone/HeadsUpAppearanceControllerTest.java
@@ -99,6 +99,7 @@ public class HeadsUpAppearanceControllerTest extends SysuiTestCase {
                 mHeadsUpStatusBarView,
                 new View(mContext),
                 mOperatorNameView,
+                new View(mContext),
                 new View(mContext));
         mHeadsUpAppearanceController.setAppearFraction(0.0f, 0.0f);
     }
@@ -179,6 +180,7 @@ public class HeadsUpAppearanceControllerTest extends SysuiTestCase {
                 mStackScrollerController,
                 mPanelView,
                 mHeadsUpStatusBarView,
+                new View(mContext),
                 new View(mContext),
                 new View(mContext),
                 new View(mContext));
