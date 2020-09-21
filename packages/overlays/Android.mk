301be425bdb482ce1ae9e9996e42ed109219c121
Chirayu Desai <chirayudesai1@gmail.com>
2020-09-22 05:05:30 +0530

    Build IconShapeRoundOverlay

--- packages/overlays/Android.mk
+++ packages/overlays/Android.mk
@@ -43,6 +43,7 @@ LOCAL_REQUIRED_MODULES := \
 	IconPackRoundedSettingsOverlay \
 	IconPackRoundedSystemUIOverlay \
 	IconPackRoundedThemePickerOverlay \
+	IconShapeRoundOverlay \
 	IconShapeRoundedRectOverlay \
 	IconShapeSquircleOverlay \
 	IconShapeTeardropOverlay \
