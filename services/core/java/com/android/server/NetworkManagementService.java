5296cc5351f654fe4ef486f536d43978900fe0db
Chirayu Desai <chirayudesai1@gmail.com>
2023-09-04 03:14:17 +0530

    Revert "Let Firewall app uid call setFirewallUidRule"
    This reverts commit ff6fab043e502cfd757d3f079fecc119aa6fc314.
    
    Reason for revert: Not needed with switch to POLICY_REJECT_ALL
    
    Change-Id: Ic639c17adc0187eae62f87e963a86846c5ef9bdb

--- services/core/java/com/android/server/NetworkManagementService.java
+++ services/core/java/com/android/server/NetworkManagementService.java
@@ -1598,7 +1598,7 @@ public class NetworkManagementService extends INetworkManagementService.Stub {
 
     @Override
     public void setFirewallUidRule(int chain, int uid, int rule) {
-        enforceSystemOrFirewallAppUid();
+        enforceSystemUid();
         synchronized (mQuotaLock) {
             setFirewallUidRuleLocked(chain, uid, rule);
         }
@@ -1685,13 +1685,6 @@ public class NetworkManagementService extends INetworkManagementService.Stub {
         final int uid = mDeps.getCallingUid();
         if (uid != Process.SYSTEM_UID) {
             throw new SecurityException("Only available to AID_SYSTEM");
-        }
-    }
-
-    private void enforceSystemOrFirewallAppUid() {
-        final int uid = mDeps.getCallingUid();
-        if (uid != Process.SYSTEM_UID && uid != Process.FIREWALL_APP_UID) {
-            throw new SecurityException("Only available to AID_SYSTEM or AID_FIREWALL_APP");
         }
     }
 
